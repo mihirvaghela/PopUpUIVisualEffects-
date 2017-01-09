b20157d462aeea67cd2c84145c5f93bbbd4e54c0
Mihir <mihir@mihirs-macbook-pro.local>
2017-01-09 05:18:41 -0500

    Phase 1 Finish

--- PopUpUIVisualEffects/ViewController.swift
+++ PopUpUIVisualEffects/ViewController.swift
@@ -14,13 +14,60 @@ class ViewController: UIViewController {
 
     @IBOutlet var addItemView: UIView!
     
-    override func viewDidLoad() {
+    var effect = UIVisualEffect()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        effect = visualView.effect!
+        visualView.effect = nil
+        
+        
+        addItemView.layer.cornerRadius = 5
+        
+        
     }
-    @IBAction func addItem(_ sender: Any) {
+    
+    func animateIn()
+    {
+        self.view.addSubview(addItemView)
+        
+        addItemView.center = self.view.center
+        
+        addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addItemView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4)
+        {
+            self.visualView.effect = self.effect
+            self.addItemView.alpha = 1
+            self.addItemView.transform = CGAffineTransform.identity
+        }
     }
-    @IBAction func dismissPopUp(_ sender: Any) {
+    
+    func animateOut()
+    {
+        UIView.animate(withDuration: 0.3, animations:
+            {
+                self.addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+                self.addItemView.alpha = 0
+                
+                self.visualView.effect = nil
+                
+        }) { (success:Bool) in
+            self.addItemView.removeFromSuperview()
+        }
+    }
+    
+    
+    @IBAction func addItem(_ sender: Any)
+    {
+        animateIn()
+    }
+    @IBAction func dismissPopUp(_ sender: Any)
+    {
+        animateOut()
     }
 
     override func didReceiveMemoryWarning() {
