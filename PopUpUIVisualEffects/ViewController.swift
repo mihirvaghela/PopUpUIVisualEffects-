4273881643b4cbe3f0ada4bc2f3f5b4b43013bc4
Mihir <mihir@mihirs-macbook-pro.local>
2017-01-09 04:46:20 -0500

    Add Navigation Controller and Add Bar Button

--- PopUpUIVisualEffects/ViewController.swift
+++ PopUpUIVisualEffects/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var visualView: UIVisualEffectView!
+
+    @IBOutlet var addItemView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func addItem(_ sender: Any) {
+    }
+    @IBAction func dismissPopUp(_ sender: Any) {
     }
 
     override func didReceiveMemoryWarning() {
